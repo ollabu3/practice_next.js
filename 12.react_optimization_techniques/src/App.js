0af44ff942891dbaaaede1fc5fdc4fef717e1514
ollabu3 <ollabu3@gmail.com>
2022-07-18 15:22:18 +0900

    useCallback 작동 closure 이헤

--- 12.react_optimization_techniques/src/App.js
+++ 12.react_optimization_techniques/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
@@ -7,18 +7,33 @@ import Output from "./components/demo/Output";
 function App() {
   console.log("APP");
   const [show, setShow] = useState(false);
+  const [allowShow, setAllowShow] = useState(false);
 
-  const handleToggleBtn =
-    (() => {
+  const handleToggleBtn = useCallback(() => {
+    console.log("handleToggleBtn");
+    if (allowShow) {
+      console.log("inner");
       setShow((show) => !show);
-    },
-    []);
+    }
+  }, [allowShow]);
+
+  const allowToggleHandler = () => {
+    console.log("allowToggleHandler");
+    setAllowShow((allowShow) => !allowShow);
+  };
+  console.log({ show });
+  console.log({ allowShow });
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <Output show={show} />
-      <Button onClick={handleToggleBtn}>TOGGLE</Button>
+      <div>
+        <Button onClick={allowToggleHandler}>ALLOW TOGGLE</Button>
+      </div>
+      <div>
+        <Button onClick={handleToggleBtn}>TOGGLE</Button>
+      </div>
     </div>
   );
 }
