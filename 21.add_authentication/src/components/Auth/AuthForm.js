c9f9c8bbddd6b55dc56ba63af397955e51f630d2
ollabu3 <ollabu3@gamil.com>
2022-10-18 02:47:23 +0900

    firebase authentication 로그인, 회원가입 로직 추가

---
+++ 21.add_authentication/src/components/Auth/AuthForm.js
@@ -0,0 +1,93 @@
+import { useRef, useState } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (e) => {
+    console.log("submit");
+    e.preventDefault();
+
+    const enterdEmail = emailInputRef.current.value;
+    const enterdPassword = passwordInputRef.current.value;
+    setIsLoading(true);
+    let url = "";
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+    } else {
+      // 로그인 안돼있으면
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enterdEmail,
+        password: enterdPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          res.json().then((_) => {
+            let errorMessage = "인증 실패!";
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p className={classes.auth}>Loading....</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
