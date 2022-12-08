645a59caab921f554845d5ed0064bba9c0fb1e39
ollabu3 <ollabu3@gmail.com>
2022-12-08 18:28:14 +0900

    SSG, SSR, getStaticPaths

--- 23.nextjs/components/meetups/MeetupDetail.js
+++ 23.nextjs/components/meetups/MeetupDetail.js
@@ -1,15 +1,34 @@
 import { Fragment } from "react";
 import styles from "./meetupdetail.module.css";
 
-const MeetupDetail = ({ image, title, address, descirption }) => {
+const MeetupDetail = ({ meetupData }) => {
   return (
     <section className={styles.detail}>
-      <img className={styles.a} src={image} alt={title} />
-      <h1>{title}</h1>
-      <address>{address}</address>
-      <p>{descirption}</p>
+      <img className={styles.a} src={meetupData.image} alt={meetupData.title} />
+      <h1>{meetupData.title}</h1>
+      <address>{meetupData.address}</address>
+      <p>{meetupData.descirption}</p>
     </section>
   );
 };
+
+export async function getStaticPaths() {}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log({ meetupId });
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "title",
+        address: "address",
+        descirption: "descirption",
+      },
+    },
+  };
+}
 
 export default MeetupDetail;
--- 23.nextjs/pages/index.js
+++ 23.nextjs/pages/index.js
@@ -1,9 +1,34 @@
 //domain/
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const HomePage = () => {
-  return <MeetupList meetups={DATA_MEETUPS} />;
+const HomePage = ({ meetups }) => {
+  return <MeetupList meetups={meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+//   // fetch
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DATA_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // 정적 페이지에서 데이터를 가져와야 할 때
+  // fetch
+  return {
+    //항상 객체를 반환한다
+    props: {
+      meetups: DATA_MEETUPS,
+    },
+    revalidate: 1, // 페이지에 요청이 새로 들어오면 10초에서 서버를 다시 생성
+  };
+}
 
 export default HomePage;
 
